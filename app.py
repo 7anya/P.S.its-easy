d3a749b2fc203762ca8e16dfdf63dbd6da8433ee
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-05-01 20:41:11 +0530

    Server statistics route added at /statistics

--- app.py
+++ app.py
@@ -2,15 +2,40 @@ from flask import Flask, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 import os
 from datetime import timedelta
-
 # decorator for routes that should be accessible only by logged in users
 from auth_decorator import login_required
-
-# dotenv setup
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_statistics import Statistics
 
 # App config
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db = SQLAlchemy(app)
+
+
+class Request(db.Model):
+    __tablename__ = "request"
+
+    index = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    response_time = db.Column(db.Float)
+    date = db.Column(db.DateTime)
+    method = db.Column(db.String)
+    size = db.Column(db.Integer)
+    status_code = db.Column(db.Integer)
+    path = db.Column(db.String)
+    user_agent = db.Column(db.String)
+    remote_address = db.Column(db.String)
+    exception = db.Column(db.String)
+    referrer = db.Column(db.String)
+    browser = db.Column(db.String)
+    platform = db.Column(db.String)
+    mimetype = db.Column(db.String)
+
+
+statistics = Statistics(app, db, Request)
+
 # Session config
 app.secret_key = "eeeeeeeeeeeeeeeeeeeeeeee"
 app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
@@ -66,3 +91,7 @@ def logout():
     for key in list(session.keys()):
         session.pop(key)
     return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run()
