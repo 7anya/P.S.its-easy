454e7f0a123d7a477d39ea029f268aa5ffcb3497
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-05-01 19:32:00 +0530

    Oauth routes made

--- app.py
+++ app.py
@@ -1,20 +1,68 @@
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, url_for, session
+from authlib.integrations.flask_client import OAuth
+import os
+from datetime import timedelta
 
-import json
+# decorator for routes that should be accessible only by logged in users
+from auth_decorator import login_required
 
+# dotenv setup
+()
+
+
+# App config
 app = Flask(__name__)
+# Session config
+app.secret_key = "eeeeeeeeeeeeeeeeeeeeeeee"
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+
+# oAuth Setup
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id="1053080488870-u4r40p6eqkc30ori7hbmh97gds5dpdfc.apps.googleusercontent.com",
+    client_secret="SAe9CnsKCmdqiNJM-AJ2XTte",
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
 
 
-#
-@app.after_request
-def add_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers[
-        'Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-    response.headers['Access-Control-Allow-Methods'] = "POST, GET, PUT, DELETE, OPTIONS"
-    return response
+@app.route('/')
+@login_required
+def hello_world():
+    email = dict(session)['profile']['email']
+    return f'Hello, you are logged in as {email}!'
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/login')
+def login():
+    google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')  # create the google oauth client
+    token = google.authorize_access_token()  # Access token from google (needed to get user info)
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+    user_info = resp.json()
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
