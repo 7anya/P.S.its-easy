348a2180c0e09fd644e671bed527918b12bf8aed
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-05-01 18:46:27 +0530

    Lets gooooo. First commit.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+import json
+
+app = Flask(__name__)
+
+
+#
+@app.after_request
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers[
+        'Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+    response.headers['Access-Control-Allow-Methods'] = "POST, GET, PUT, DELETE, OPTIONS"
+    return response
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
