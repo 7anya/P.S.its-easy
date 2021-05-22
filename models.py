e3ecdb57241f057b01466135fa0f71ff2432fdfb
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-05-22 12:33:23 +0530

    users entered into db

--- models.py
+++ models.py
@@ -12,10 +12,38 @@ print(records.count_documents({}))
 
 allData = list(records.find())
 for x in allData:
-    x['_id']=0
+    x['_id'] = 0
 # print(allData)
 
 chronicles = json.dumps(allData)
 
-# print(chronicles)
+userdb = client.get_database("users")
+users = userdb.users
 
+
+def insertIfDoesntExist(userinfo):
+    finduser = list(users.find({"email": userinfo['email'], "verified_email": True}))
+    print("number of users", len(finduser))
+    email = userinfo['email']
+    if len(finduser) == 0:
+        if email[10] == 'h':
+            campus = "hyderabad"
+        elif email[10] == 'g':
+            campus = "goa"
+        else:
+            campus = "pilani"
+        userinfo['campus'] = campus
+        print(type(userinfo))
+        foc = {
+            "id": userinfo['id'],
+            "email": userinfo['email'],
+            "verified_email": userinfo["verified_email"],
+            "name": userinfo['name'],
+            "given_name": userinfo["given_name"],
+            "picture": userinfo["picture"],
+            "locale": userinfo["locale"],
+            "hd": userinfo["hd"],
+            "campus": userinfo["campus"]
+        }
+
+        users.insert_one(foc)
