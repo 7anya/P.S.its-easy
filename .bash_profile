1adc6ab562b85aa9fabe5ec194a46e7ffa84fe06
root <root@everyday-use-1.vcn01260153.oraclevcn.com>
2021-05-28 23:23:51 +0530

    Trying to configure ssh (incomplete)

---
+++ .bash_profile
@@ -0,0 +1,11 @@
+# .bash_profile
+
+# If .bash_profile exists, bash doesn't read .profile
+if [[ -f ~/.profile ]]; then
+  . ~/.profile
+fi
+
+# If the shell is interactive and .bashrc exists, get the aliases and functions
+if [[ $- == *i* && -f ~/.bashrc ]]; then
+    . ~/.bashrc
+fi
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-cd
-apk add gcc g++ musl-dev python3-dev py3-pip libressl-dev
+# Go to home directory (/root), where the repo is
+cd 
+
+# Install all required applications
+apk add --no-cache gcc g++ musl-dev python3-dev py3-pip libressl-dev libffi-dev bash openssh sed
+
+# Configure and enable ssh
+# sed -i 's/#Port\ 22/Port\ 22/' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication\ yes/PubkeyAuthentication\ yes/' /etc/ssh/sshd_config
+sed -i 's/#HostKey\ /etc/ssh/ssh_host_rsa_key/HostKey\ /etc/ssh/ssh_host_rsa_key/' /etc/ssh/sshd_config
+sed -i 's/#HostKey\ /etc/ssh/ssh_host_ecdsa_key/HostKey\ /etc/ssh/ssh_host_ecdsa_key/' /etc/ssh/sshd_config
+sed -i 's/AllowTcpForwarding\ no/AllowTcpForwarding\ yes/' /etc/ssh/sshd_config
+
+cp .ssh/ssh_host_* /etc/ssh/
+/usr/sbin/sshd
+
+# Set bash as default
+sed -i 's/ash/bash/' /etc/passwd
+
+# Install dependencies and run the flask server
 pip install -r requirements.txt -I
-python3 app.py
+python3 app.py 
+
