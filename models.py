a6d0e88d98490e9cdd8d3cf2bfe4e6ae9598b85d
Joe2k <jonathansamuel2k@gmail.com>
2021-10-31 23:49:47 +0530

    [FIX] Hactoberfest

--- models.py
+++ models.py
@@ -173,7 +173,7 @@ def form_detailed_responses():
 userdb = client.get_database("users")
 users = userdb.users
 
-# hello
+# hel
 
 def insertIfDoesntExist(userinfo):
     finduser = list(users.find({"email": userinfo['email'], "verified_email": True}))
