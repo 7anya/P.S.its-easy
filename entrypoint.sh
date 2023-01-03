853d9e97f0bddc4a8632e90d06a620f45ca5533b
Rut Vora <contactme@rutvora.com>
2023-01-04 04:14:40 +0530

    Updated entrypoint script + keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,11 @@
 #!/bin/sh
 
 # Go to home directory (/root), where the repo is
-cd 
+cd
 
-# Install all required applications
-apk add --no-cache gcc g++ musl-dev python3-dev py3-pip libressl-dev libffi-dev bash openssh sed
-apk add --no-cache nano git
-
-# Configure and enable ssh
-# sed -i 's/#Port\ 22/Port\ 22/' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication\ yes/PubkeyAuthentication\ yes/' /etc/ssh/sshd_config
-sed -i 's/#HostKey\ /etc/ssh/ssh_host_rsa_key/HostKey\ /etc/ssh/ssh_host_rsa_key/' /etc/ssh/sshd_config
-sed -i 's/#HostKey\ /etc/ssh/ssh_host_ecdsa_key/HostKey\ /etc/ssh/ssh_host_ecdsa_key/' /etc/ssh/sshd_config
-sed -i 's/AllowTcpForwarding\ no/AllowTcpForwarding\ yes/' /etc/ssh/sshd_config
-sed -i 's/#PermitRootLogin\ prohibit-password/PermitRootLogin\ prohibit-password/' /etc/ssh/sshd_config
-cp .ssh/ssh_host_* /etc/ssh/
-chmod 400 /etc/ssh/ssh_host_*
+mkdir /run/sshd
 /usr/sbin/sshd
-passwd -u root # Enables passwordless access via ssh
 
-# Set bash as default
-sed -i 's/ash/bash/' /etc/passwd
-
-# Install dependencies and run the flask server
-pip install -r requirements.txt -I
-# python3 app.py &
 gunicorn -w 4 -b :5000 app:app &
 
 # Change /etc/motd to change login message to notify the user that this is a docker container
